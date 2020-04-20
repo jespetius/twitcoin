c4454254a63787922f7df8c7123da7785f37f63d
jespetius74 <jeppe.kuula@gmail.com>
2020-04-20 13:06:05 +0300

    user as json

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, request, jsonify, render_template
+import requests
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/json', methods=['GET', 'POST'])
+def json():
     testi = "testi"
     return jsonify({"key" : testi})
+
+
+@app.route('/user', methods=['POST'])
+def user():
+    user = request.form['userform']
+    return jsonify({"key" : user})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
