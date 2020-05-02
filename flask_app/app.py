3481115c6c9c810dd832b301347f0f6c06b9387c
OMikk <ossimikkonen@hotmail.com>
2020-05-02 15:23:08 +0300

    try scriptit flaskiin

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,22 +1,30 @@
-#testaus pelkka flask
+
+# testaus pelkka flask
 from flask import Flask, request, jsonify, render_template
+from tweets_get import sentiment_analysis
 import requests
 app = Flask(__name__)
 
-#json testi
+# json testi
 @app.route('/json', methods=['GET', 'POST'])
 def json():
     testi = "testi"
-    return jsonify({"key" : testi})
+    return jsonify({"key": testi})
 
-#user jsonina 
+# user jsonina
 @app.route('/user', methods=['POST'])
 def user():
     user = request.form['userform']
-    return jsonify({"key" : user})
+    return jsonify({"key": user})
 
-#index
+
+@app.route('/search', methods=['POST'])
+def search():
+    # user_search = request.form['searchform']
+    # user_search = user
+    return sentiment_analysis()
+
+# index
 @app.route('/')
 def index():
     return render_template('index.html')
-
